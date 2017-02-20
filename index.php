bc796ad4fc10c730b1fc396a68943adbe84660f7
Michel Neven <michel@minesco.ch>
2017-02-20 20:21:16 +0100

    Initial commit. Add index, sass, compass.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+	<link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<link href="/stylesheets/spencer.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+  <!--[if IE]>
+  	<link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <![endif]-->
+  <title>Spencer | Your mobile workplace assistant</title>
+</head>
+<body>
+
+</body>
+</html>
